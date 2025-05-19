f9579576303df1f1894e740ea3f6e8ae476efa6e
Théo Davigny <davigny.theo@gmail.com>
2025-05-19 21:18:51 +0200

    add login, register and profile

---
+++ back/src/auth/auth.controller.ts
@@ -0,0 +1,21 @@
+import { Controller, Post, Body, ValidationPipe, Req, Get, UseGuards } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login.dto';
+import { JwtAuthGuard } from './jwt-auth.guard';
+import { Request } from 'express';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(@Body(new ValidationPipe()) dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('validate')
+  validateToken(@Req() req: Request) {
+    return { valid: true };
+  }
+}
